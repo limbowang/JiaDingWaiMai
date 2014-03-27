ec7ca6c538cc306445e36b454532ef62418c23e4
van <van8701@gmail.com>
2014-03-28 01:13:14 +0800

    merge

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@
 (function() {
     $("#search-box").bind('input propertychange', function() {
         console.log($(this).val());
-    })
+    });
 }());
 
 function OnSearchInput(event){
   var content = event.target.value;
-  var items = $("#sidebar-list li");
+  var items = $("#sidebar-list").find("li");
   for(var i = 0; i < items.length; i++) {
     if(items.eq(i).text().indexOf(content) < 0){
       items.eq(i).hide();
