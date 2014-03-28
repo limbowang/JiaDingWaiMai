12dbf12f1263772a2a57026d1e5417c817fa0b16
van <van8701@gmail.com>
2014-03-28 13:26:48 +0800

    Solve sidebar scrolling problem.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,33 @@
  * Created by Limbo on 14-3-26.
  */
 
-(function() {
-    $("#search-box").bind('input propertychange', function() {
-        console.log($(this).val());
+$(document).ready(function () {
+    $(window).bind('load resize', function () {
+        $(".sidebar").each(function () {
+            var height = $(this).height() - $(this).find(".sidebar-header").height();
+            $(".sidebar-content").css("height", height - 20);
+        })
     });
-}());
 
-function OnSearchInput(event){
-  var content = event.target.value;
-  var items = $("#sidebar-list").find("li");
-  for(var i = 0; i < items.length; i++) {
-    if(items.eq(i).text().indexOf(content) < 0){
-      items.eq(i).hide();
-    }else{
-      items.eq(i).show();
-    }
-  }
-}
+    $(".sidebar-content").bind("mousewheel", function (e) {
+        e.preventDefault();
+        var d = e.originalEvent.wheelDelta;
+        var sidebarList = $(this).find(".sidebar-list");
+        var top = sidebarList.position().top;
+        var visibleHeight = sidebarList.height() + top;
+        if (!(d > 0 && top == 0) && !(d < 0 && visibleHeight < $(this).height()))
+            sidebarList.css("top", top + d);
+    });
+
+    $("#search-box").bind('input propertychange', function () {
+        var input = $(this).val();
+        $("#sidebar-list").find("li").each(function () {
+            if ($(this).text().indexOf(input) < 0) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        })
+    });
+});
 
