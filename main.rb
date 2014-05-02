a7d089ac57e3e1913b3b0a9cb0a082ffe4a4d6a3
tzwm <wangjunzhe4444@gmail.com>
2014-05-02 23:36:12 +0000

    Refactor the project for directory and remove tools writen in JS.

--- main.rb
+++ main.rb
@@ -1,41 +1,57 @@
 require 'slim'
 require 'json'
+require 'fileutils'
 
-DATA_DIR = "data/merged/"
-TEMPLATES_DIR = "templates/"
+$DATA_DIR = "./data/merged"
+$LAYOUTS_DIR = "./layouts"
+$DESTINATION = "./_site"
 
 class Content
   attr_accessor :items, :sidebar,
-                :contents_left, :contents_right
+    :contents_left, :contents_right
 end
 
-data = Content.new
+def init
+  if not Dir.exist? $DESTINATION
+    Dir.mkdir $DESTINATION
+  end
 
-items = File.read(DATA_DIR + "mergedData.json")
-items = JSON.parse(items)
-data.items = items["items"]
+  FileUtils.cp_r "./js", $DESTINATION   
+  FileUtils.cp_r "./css", $DESTINATION   
+  FileUtils.cp_r "./img", $DESTINATION   
+end
 
-data.sidebar = Slim::Template.new(TEMPLATES_DIR+"sidebar-list.slim")
-                              .render(data)
+def generateIndex
+  data = Content.new
 
-items = File.read(DATA_DIR + "contents-left.json")
-items = JSON.parse(items)
-data.items = items["items"]
+  items = File.read "#{$DATA_DIR}/mergedData.json"
+  items = JSON.parse items
+  data.items = items["items"]
+  data.sidebar = Slim::Template.new("#{$LAYOUTS_DIR}/sidebar-list.slim")
+    .render(data)
 
-data.contents_left = Slim::Template.new(TEMPLATES_DIR+"contents.slim")
-                                       .render(data)
+  items = File.read "#{$DATA_DIR}/contents-left.json"
+  items = JSON.parse items
+  data.items = items["items"]
+  data.contents_left = Slim::Template.new("#{$LAYOUTS_DIR}/contents.slim")
+    .render(data)
 
+  items = File.read "#{$DATA_DIR}/contents-right.json"
+  items = JSON.parse items
+  data.items = items["items"]
+  data.contents_right = Slim::Template.new("#{$LAYOUTS_DIR}/contents.slim")
+    .render(data)
 
-items = File.read(DATA_DIR + "contents-right.json")
-items = JSON.parse(items)
-data.items = items["items"]
+  index = Slim::Template.new("#{$LAYOUTS_DIR}/index.slim").render(data)
+  file = File.new "#{$DESTINATION}/index.html", "w"
+  file.print index 
+  file.close
+end
 
-data.contents_right = Slim::Template.new(TEMPLATES_DIR+"contents.slim")
-                                        .render(data)
+def main
+  init
+  generateIndex 
+end
 
+main
 
-output = Slim::Template.new("index.slim").render(data)
-
-file = File.new("index.html", "w")
-file.print output
-file.close
