07d1fcfcdaba50aa9640100596aee2a1f84750b7
tzwm <wangjunzhe4444@gmail.com>
2014-05-04 18:25:47 +0000

    Add Thor Gem for CLI.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'http://ruby.taobao.org'
 
 gem 'slim'
 gem 'json'
-
+gem 'thor'
--- main.rb
+++ main.rb
@@ -1,60 +1,86 @@
+#!/usr/bin/env ruby
+
 require 'slim'
 require 'json'
 require 'fileutils'
+require 'thor'
 
-$DATA_DIR = "./data/merged"
-$LAYOUTS_DIR = "./layouts"
-$DESTINATION = "./_site"
+DATA_DIR = "./data/merged"
+LAYOUTS_DIR = "./layouts"
+DESTINATION = "./_site"
 
 class Content
   attr_accessor :items, :sidebar,
     :contents_left, :contents_right
 end
 
-def init
-  if not Dir.exist? $DESTINATION
-    Dir.mkdir $DESTINATION
+class MyCommand < Thor
+  desc "build", "Build your site."
+  def build
+    init
+    moveDir
+    generateIndex
   end
 
-  FileUtils.rm_r Dir.glob("#{$DESTINATION}/*")
+  desc "merge", "Merge the data."
+  def merge
+    merge
+  end
 
-  FileUtils.cp_r "./js", $DESTINATION   
-  FileUtils.cp_r "./css", $DESTINATION   
-  FileUtils.cp_r "./img", $DESTINATION   
-  FileUtils.cp_r "./fonts", $DESTINATION   
+  desc "compress", "Compress pending images."
+  def compress
+    compress
+  end
+end
+
+
+def init
+  if not Dir.exist? DESTINATION
+    Dir.mkdir DESTINATION
+  end
+
+  FileUtils.rm_r Dir.glob("#{DESTINATION}/*")
+end
+
+def moveDir 
+  needCP = ["./js", "./css", "./img", "./fonts"]
+  FileUtils.cp_r needCP, DESTINATION 
 end
 
 def generateIndex
   data = Content.new
 
-  items = File.read "#{$DATA_DIR}/mergedData.json"
+  items = File.read "#{DATA_DIR}/mergedData.json"
   items = JSON.parse items
   data.items = items["items"]
-  data.sidebar = Slim::Template.new("#{$LAYOUTS_DIR}/sidebar-list.slim")
+  data.sidebar = Slim::Template.new("#{LAYOUTS_DIR}/sidebar-list.slim")
     .render(data)
 
-  items = File.read "#{$DATA_DIR}/contents-left.json"
+  items = File.read "#{DATA_DIR}/contents-left.json"
   items = JSON.parse items
   data.items = items["items"]
-  data.contents_left = Slim::Template.new("#{$LAYOUTS_DIR}/contents.slim")
+  data.contents_left = Slim::Template.new("#{LAYOUTS_DIR}/contents.slim")
     .render(data)
 
-  items = File.read "#{$DATA_DIR}/contents-right.json"
+  items = File.read "#{DATA_DIR}/contents-right.json"
   items = JSON.parse items
   data.items = items["items"]
-  data.contents_right = Slim::Template.new("#{$LAYOUTS_DIR}/contents.slim")
+  data.contents_right = Slim::Template.new("#{LAYOUTS_DIR}/contents.slim")
     .render(data)
 
-  index = Slim::Template.new("#{$LAYOUTS_DIR}/index.slim").render(data)
-  file = File.new "#{$DESTINATION}/index.html", "w"
+  index = Slim::Template.new("#{LAYOUTS_DIR}/index.slim").render(data)
+  file = File.new "#{DESTINATION}/index.html", "w"
   file.print index 
   file.close
 end
 
-def main
-  init
-  generateIndex 
+def merge
+
 end
 
-main
+def compress
 
+end
+
+
+MyCommand.start
