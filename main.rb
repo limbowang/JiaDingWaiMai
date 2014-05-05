2e439427a41d9e3ba0ac57e30fcb69cc911a0272
tzwm <wangjunzhe4444@gmail.com>
2014-05-05 16:49:38 +0000

    Add website-tracking.js and add function to upload img in main.rb.

--- main.rb
+++ main.rb
@@ -5,26 +5,11 @@ require 'json'
 require 'fileutils'
 require 'thor'
 require 'mini_magick'
+require 'net/http'
+require 'base64'
+require 'openssl'
+require 'rest_client'
 
-
-class MyCommand < Thor
-  desc "build", "Build your site."
-  def build
-    JiaDingWaiMai.init
-    JiaDingWaiMai.moveDir
-    JiaDingWaiMai.generateIndex
-  end
-
-  desc "merge", "Merge the data."
-  def merge
-    JiaDingWaiMai.merge
-  end
-
-  desc "compress", "Compress pending images."
-  def compress
-    JiaDingWaiMai.compress
-  end
-end
 
 module JiaDingWaiMai
   DATA_DIR = "./data"
@@ -79,7 +64,7 @@ module JiaDingWaiMai
     file.close
   end
 
-  def merge
+  def mergeData
     items = Array.new
     Dir.foreach( "#{DATA_DIR}/json" ) do |filename|
       next if File.extname(filename).downcase != ".json"
@@ -98,7 +83,7 @@ module JiaDingWaiMai
     puts "Data merged into #{DATA_DIR}/merged/mergedData.json"
   end
 
-  def compress(img_width_small=415, img_width_large=1024)
+  def compressImg(img_width_small=415, img_width_large=1024)
     num = 0
     Dir.foreach("#{DATA_DIR}/img/pending") do |filename|
       next if not File.extname(filename).downcase =~ /\.jpg|\.png/
@@ -118,7 +103,66 @@ module JiaDingWaiMai
     puts "#{num} images compressed."
   end
 
-  module_function :init, :moveDir, :generateIndex, :merge, :compress
+  def uploadImg bucket_name = "jiadingwaimai"
+    accessKey = "ysm4KDdDLab4Q777LjVvxe_jhDfG7GDhorOSkcAP"
+    secretKey = "jcUs2RW2pb3dDCRIBG92CFT_8kXVIyFe--s6B5sO"
+
+    num = 0
+    uri = "http://up.qiniu.com/"
+    digest = OpenSSL::Digest.new('sha1')
+    Dir.foreach("#{DATA_DIR}/img/done") do |filename|
+      next if not File.extname(filename).downcase =~ /\.jpg|\.png/
+
+      num +=1
+      putPolicy = Hash.new
+      putPolicy[:scope] = "#{bucket_name}:#{filename}"
+      putPolicy[:deadline] = Time.now.to_i + 60 * 60
+
+      putPolicyJSON = JSON.generate putPolicy
+      encodedPutPolicy = Base64.strict_encode64 putPolicyJSON
+      sign = OpenSSL::HMAC.digest(digest, secretKey, encodedPutPolicy)
+      encodedSign = Base64.strict_encode64 sign
+      encodedSign.gsub!(/\+/, "-")
+      uploadToken = "#{accessKey}:#{encodedSign}:#{encodedPutPolicy}"
+      RestClient.post(uri,
+                      {:token => uploadToken,
+                       :file => File.new("#{DATA_DIR}/img/done/#{filename}", "rb"),
+                       :key => filename,
+                       :multipart => true})
+      puts "#{filename} uploaded."
+    end
+
+    puts "#{num} imgaes uploaded."
+  end
+
+  module_function :init, :moveDir, :generateIndex, :mergeData, :compressImg
+end
+
+
+class MyCommand < Thor
+  include JiaDingWaiMai
+
+  desc "build", "Build your site."
+  def build
+    init
+    moveDir
+    generateIndex
+  end
+
+  desc "merge", "Merge the data."
+  def merge
+    mergeData
+  end
+
+  desc "compress", "Compress pending images."
+  def compress
+    compressImg
+  end
+
+  desc "upload", "Upload compressed images."
+  def upload
+    uploadImg
+  end
 end
 
 
